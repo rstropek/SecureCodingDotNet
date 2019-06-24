789f47758b0dd88bf0274f53e42982b332c0a0cb
Rainer Stropek <rainer@software-architects.at>
2019-06-24 18:39:15 +0200

    Add samples for dotnet secret manager tool

---
+++ 09-ProtectSettings/09.1-SecretManager/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace RecapNetCoreConfig
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile(Path.Combine(".", "appsettings.json"));
+
+            // Add user secrets from running assembly.
+            // Note that this is not necessary in ASP.NET Core apps because
+            // this is done automatically by CreateDefaultBuilder
+            // (for details see https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.2&tabs=windows#access-a-secret).
+            builder.AddUserSecrets(Assembly.GetExecutingAssembly());
+
+            var config = builder.Build();
+            Console.WriteLine($"Foo has value {config["foo"]}.");
+            Console.WriteLine($"Ping has value {config["ping"]}.");
+        }
+    }
+}
